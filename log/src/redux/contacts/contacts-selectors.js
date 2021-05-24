d3c046dbff03ef39696ffb6b15dd3449afc9913a
SergiyStetsenko <celtik45@gmail.com>
2021-05-24 14:46:35 +0300

    cgvjbhkjn

--- log/src/redux/contacts/contacts-selectors.js
+++ log/src/redux/contacts/contacts-selectors.js
@@ -6,17 +6,18 @@ const getFilter = (state) => state.contacts.filter;
 
 const getAllContacts = (state) => state.contacts.items;
 
-export const getVisibleContacts = createSelector(
-  [getAllContacts, getFilter],
-  (contacts, filter) => {
-    const normalizedFilter = filter.toLowerCase();
+const getVisibleContacts = state => {
+  const contacts = getAllContacts(state);
+  const filter = getFilter(state);
 
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  }
-);
+  const normalizedFilter = filter.toLowerCase();
 
+  return contacts.filter(({ name}) =>
+    name.toLowerCase().includes(normalizedFilter),
+  );
+};
+
+  
 export default {
   getLoading,
   getFilter,
